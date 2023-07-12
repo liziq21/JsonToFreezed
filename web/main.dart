5f2477169cdcea92ceb0739d682795e5581c5518
Chhunvirak <Chhunvirakchhoeung@gmail.com>
2023-07-13 01:41:06 +0700

    update UI and Process

--- web/main.dart
+++ web/main.dart
@@ -1,88 +1,105 @@
-import 'dart:html';
 import 'dart:convert';
+import 'dart:html';
 
-import 'utils/freezed_syntax.dart';
+import 'utils/class_builder.dart';
 import 'utils/string_util.dart';
 
 Future<void> main() async {
   ///TextArea
-  TextAreaElement a = document.getElementById('mytextfield') as TextAreaElement;
+  TextAreaElement? inputJson =
+      document.getElementById('input__json') as TextAreaElement?;
   InputElement inputClassName =
-      document.getElementById('class-name') as InputElement;
+      document.getElementById('input__classname') as InputElement;
 
-  Element? btn = document.getElementById("btn");
+  ButtonElement? btnGenerate =
+      document.getElementById("btn__generate") as ButtonElement?;
+
+  ButtonElement? btnCopy =
+      document.getElementById("btn__copy") as ButtonElement?;
+
   CheckboxInputElement stringBTN =
-      document.getElementById("String") as CheckboxInputElement;
+      document.getElementById("string") as CheckboxInputElement;
   CheckboxInputElement intBTN =
       document.getElementById("int") as CheckboxInputElement;
   CheckboxInputElement doubleBTN =
       document.getElementById("double") as CheckboxInputElement;
   CheckboxInputElement booleanBTN =
       document.getElementById("boolean") as CheckboxInputElement;
+  CheckboxInputElement listBTN =
+      document.getElementById("list") as CheckboxInputElement;
 
-  SelectElement choice =
-      document.getElementById('boolean-choice') as SelectElement;
+  SelectElement boolOptionValue =
+      document.getElementById("option__box-boolen-value") as SelectElement;
 
-  bool enabledDefault() =>
-      stringBTN.checked! ||
-      intBTN.checked! ||
-      doubleBTN.checked! ||
-      booleanBTN.checked!;
+  // SelectElement choice =
+  //     document.getElementById('boolean-choice') as SelectElement;
+
+  // bool enabledDefault() =>
+  //     stringBTN.checked! ||
+  //     intBTN.checked! ||
+  //     doubleBTN.checked! ||
+  //     booleanBTN.checked!;
 
   // Element? copybtn = document.getElementById("copy");
 
-  bool start() {
-    String className =
+  void generate() async {
+    final className =
         inputClassName.value != null && inputClassName.value!.isNotEmpty
             ? camelCase(inputClassName.value!)
-            : '';
-    final classSyntax = FreezedSyntax(className: className);
-    String result = classSyntax.startClass();
-    String constructor = classSyntax.startConstructor();
+            : null;
 
     try {
       //
-      Map<String, dynamic> decoded = jsonDecode(a.value ?? '');
-      decoded.entries.map((e) {
-        constructor =
-            '$constructor ${enabledDefault() ? freezedSyntaxWithDefault(
-                e.key,
-                e.value,
-                enableStr: stringBTN.checked,
-                enableInt: intBTN.checked,
-                enableDouble: doubleBTN.checked,
-                enableBoolean: booleanBTN.checked,
-                boolValue: choice.selectedIndex == 0,
-              ) : freezedSyntax(e.key, e.value)}';
-      }).toList();
-      constructor = constructor + classSyntax.endConstructor();
-      result = '$result$constructor ${classSyntax.fromJsonSyntax()} \n}';
+      final classGen = JsonToFreezedGenerator(className: className);
+      final jsonInput = inputJson?.value;
 
-      ///output
-      document.querySelector('#output')?.text = result;
+      if (jsonInput != null && jsonInput != '') {
+        final dynamic decodedJson = await jsonDecode(jsonInput);
+        classGen.generateFreezedDartDataClass(
+          decodedJson: decodedJson,
+          defaultValues: DefaultValue(
+              defaultString: stringBTN.checked,
+              defaultBool: booleanBTN.checked,
+              defaultDouble: doubleBTN.checked,
+              defaultInt: intBTN.checked,
+              defaultList: listBTN.checked,
+              boolValue: boolOptionValue.value ?? 'false'),
+        );
+
+        ///output
+        document.querySelector('#output')?.text =
+            classGen.allClasses.join('\n\n');
+      } else {
+        window.alert('Please input Json in the box left to Generate.');
+      }
     } catch (e) {
-      print(e);
+      // print(e);
       if (e is FormatException) {
         window
             .alert('Json data are not correct. try recheck again ${e.message}');
       } else {
         window.alert('Something went wrong. try refresh the page again');
       }
-
-      return false;
     }
-    return true;
   }
 
-  btn?.addEventListener("click", (_) async {
-    start();
+  btnGenerate?.addEventListener("click", (_) async {
+    print('Clicked');
+
+    generate();
   });
 
-  // copybtn?.addEventListener("click", (_) async {
-  //   print('VALUE : $aa');
-  //   // aa?.clipboard?.writeText(document.querySelector('#output')?.text ?? '');
-  // });
-} //main
+  btnCopy?.addEventListener("click", (_) async {
+    final outputTexts = document.querySelector('#output')?.text ?? '';
+
+    if (outputTexts.trim().isNotEmpty) {
+      window.navigator.clipboard
+          ?.writeText(document.querySelector('#output')?.text ?? '');
+    } else {
+      window.alert('No outputs found!');
+    }
+  });
+}
 
 // ignore_for_file: constant_identifier_names
 
@@ -96,71 +113,3 @@ class ClassName with _$ClassName {
   Map<String, dynamic> toJson() => _$ClassNameToJson(this);
 }
 */
-
-class EnableDefault {
-  bool? enableString;
-  bool? enableInt;
-  bool? enableDouble;
-  bool? enableBoolean;
-}
-
-const CLASS_NAME = 'ClassName';
-
-String constr(String key) => 'this.${camelCaseFirstLower(key)},';
-
-String freezedSyntax(String key, dynamic value) =>
-    '   ${jsonKey(key)} final ${getType(value)}? ${camelCaseFirstLower(key)},\n';
-
-String freezedSyntaxWithDefault(String key, dynamic value,
-        {bool? enableStr = false,
-        bool? enableInt = false,
-        bool? enableDouble = false,
-        bool? enableBoolean = false,
-        bool? boolValue = false}) =>
-    '   ${jsonKey(key)} ${_getDefault(
-      enableStr: enableStr,
-      enableInt: enableInt,
-      enableDouble: enableDouble,
-      enableBoolean: enableBoolean,
-      boolValue: boolValue,
-      type: getType(value),
-    )}final ${getType(value)}? ${camelCaseFirstLower(key)},\n';
-
-String getType(dynamic data) {
-  if (data is List) {
-    return 'List<dynamic>';
-  } else if (data is Map) {
-    return 'Map<String,dynamic>';
-  } else {
-    return data.runtimeType.toString();
-  }
-}
-
-String jsonKey(String key) => "@JsonKey(name:\"$key\")";
-String defaultKey(key) => "@Default($key) ";
-
-const num _defaultNum = 0;
-
-String _getDefault(
-    {bool? enableStr = false,
-    bool? enableInt = false,
-    bool? enableDouble = false,
-    bool? enableBoolean = false,
-    bool? boolValue = false,
-    required String type}) {
-  String value = '';
-
-  if (type == 'String' && enableStr!) {
-    value = defaultKey("\"\"");
-  }
-  if (type == 'int' && enableInt!) {
-    value = defaultKey(_defaultNum);
-  }
-  if (type == 'double' && enableDouble!) {
-    value = defaultKey(_defaultNum);
-  }
-  if (type == 'bool' && enableBoolean!) {
-    value = defaultKey(boolValue);
-  }
-  return value;
-}
--- web/utils/freezed_syntax.dart
+++ web/utils/freezed_syntax.dart
@@ -7,16 +7,20 @@ class FreezedSyntax {
   final _freezedImport =
       "import 'package:freezed_annotation/freezed_annotation.dart';";
 
-  String get getClassName => className != null && className!.isNotEmpty
+  String get _getPartImport => '''
+part '${_getClassName.toLowerCase()}.freezed.dart';
+part '${_getClassName.toLowerCase()}.g.dart';''';
+
+  String get _getClassName => className != null && className!.isNotEmpty
       ? className!
       : _defaultClassName;
 
   String startClass() =>
-      "$_freezedImport\n\n@Freezed\nclass $getClassName with _\$$getClassName {\n";
-  String startConstructor() => 'factory $getClassName({ \n';
-  String endConstructor() => "  }) = _$getClassName;";
+      "$_freezedImport\n$_getPartImport\n\n@Freezed\nclass $_getClassName with _\$$_getClassName {\n";
+  String startConstructor() => 'factory $_getClassName({ \n';
+  String endConstructor() => "  }) = _$_getClassName;";
   String fromJsonSyntax() =>
-      "\nfactory $getClassName.fromJson(Map<String, dynamic> json) =>_\$${getClassName}FromJson(json);";
+      "\nfactory $_getClassName.fromJson(Map<String, dynamic> json) =>_\$${_getClassName}FromJson(json);";
 
   String buildClass(
       {required Map<String, dynamic> mapData, required bool enableDefault}) {
